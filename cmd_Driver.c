e19bdf47f87c91a99c2c440750f2442de2eec236
Diego <diegobailos@gmail.com>
2020-01-14 00:34:03 +0000

    Copyright text

--- cmd_Driver.c
+++ cmd_Driver.c
@@ -1,10 +1,29 @@
-/*
- * Uart_Driver.c
- *
- *  Created on: 17 Oct 2019
- *      Author: Diego B.
- */
+ /******************************************************************************
+ * @file           : Uart_Driver.c
+ * @version        : v1.0
+ * @brief          : Command process interface.
+ * @author		   : Diego B.	Oct-18
+ ******************************************************************************
+  Revisions can be found here:
+  https://github.com/litanx/CMD/
 
+  Copyright (C) diegobailos , <diegobailos@gmail.com>
+
+                                GPLv3 License
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************************
+*/
 /* Private includes ----------------------------------------------------------*/
 #include "main.h"
 #include "cmd_task.h"
